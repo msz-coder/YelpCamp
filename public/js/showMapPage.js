a9bde018108534e61af6ac0830a7d467cb2c1105
Shafay Zulfiqar <shafay@Shafays-MacBook-Air.local>
2022-08-25 04:04:08 +0300

    fixing maps on shows page

--- public/js/showMapPage.js
+++ public/js/showMapPage.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v11", // style URL
   center: campgroundCoords.geometry.coordinates, // starting position [lng, lat]
-  zoom: 9, // starting zoom
+  zoom: 8, // starting zoom
   projection: "globe", // display the map as a 3D globe
 });
 map.on("style.load", () => {
